145896cfd92534d27df06572f8b63e71c464bbd8
Shivam Tripathi <shivam.tripathi@buhlergroup.com>
2024-01-08 06:41:29 +0000

    My First UI5 App

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zlistrptshiv.ui5listrptshiv.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
